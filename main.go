cc1d16998fb629419dee6a0e4aa13cfd091a208a
sogener <sogener@yandex.ru>
2022-06-07 21:17:50 +0500

    Goroutines

--- main.go
+++ main.go
@@ -1,25 +1,36 @@
 package main
 
-import "fmt"
-
-const (
-	// Create a huge number by shifting a 1 bit left 100 places.
-	// In other words, the binary number that is 1 followed by 100 zeroes.
-	Big = 1 << 100
-	// Shift it right again 99 places, so we end up with 1<<1, or 2.
-	Small = Big >> 99
+import (
+	"fmt"
+	"time"
 )
 
-func needInt(x int) int { return x*10 + 1 }
-func needFloat(x float64) float64 {
-	return x * 0.1
+func main() {
+	t := time.Now() // время старта
+
+	fmt.Printf("Старт: %s\n", t.Format(time.RFC3339))
+
+	// очень сложные вычисления
+	go calculateSomething(1000)
+
+	// а эти еще сложнее
+	go calculateSomething(2000)
+
+	// Блокирует выполнение главной горутины.
+	// Планировщик начнёт выполнять другие не заблокированные горутины
+	time.Sleep(8 * time.Second)
+
+	fmt.Printf("Время выполнения программы: %s\n", time.Since(t))
 }
 
-func main() {
-	//fmt.Println(Big)
-	//fmt.Println(Small)
+func calculateSomething(n int) {
+	t := time.Now()
 
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+	result := 0
+	for i := 0; i <= n; i++ {
+		result += i * 2
+		time.Sleep(time.Millisecond * 3)
+	}
+
+	fmt.Printf("Результат: %d; Прошло времени: %s\n", result, time.Since(t))
 }
