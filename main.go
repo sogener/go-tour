7c04d121b93364daf0eedd4e25a53abbc97d0fdf
sogener <sogener@yandex.ru>
2022-06-07 20:43:56 +0500

    Variables

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import "fmt"
 
-func split(sum int) (x, y int) {
-	x = sum / 4
-	y = sum / 2
-	return
-}
+var c, python, java bool
 
 func main() {
-	fmt.Println(split(20))
+	var i int
+	fmt.Println(i, c, python, java)
 }
