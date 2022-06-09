86db144c8bd0a804d4c07f30309f1ffb6414fdab
sogener <sogener@yandex.ru>
2022-06-09 21:31:14 +0500

    Structs

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
-	fmt.Println("counting")
-
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-
-	fmt.Println("done")
+	fmt.Println(Vertex{1, 2})
 }
