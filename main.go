a6e26fc9453536b349e539b136cfb4e06fb8277f
sogener <sogener@yandex.ru>
2022-06-09 22:00:00 +0500

    Slice defaults

--- main.go
+++ main.go
@@ -3,22 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	q := []int{2, 3, 5, 7, 11, 13}
-	fmt.Println(q)
+	s := []int{2, 3, 5, 7, 11, 13}
 
-	r := []bool{true, false, true, true, false, true}
-	fmt.Println(r)
-
-	s := []struct {
-		i int
-		b bool
-	}{
-		{2, true},
-		{3, false},
-		{5, true},
-		{7, true},
-		{11, false},
-		{13, true},
-	}
+	s = s[1:4]
 	fmt.Println(s)
+
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
+	fmt.Println(s)
+
+	/**
+	OUTPUT:
+	[3 5 7]
+	[3 5]
+	[5]
+	*/
 }
