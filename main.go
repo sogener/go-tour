65c8819c94cf8b5df2484bcb8745de5ae2038321
sogener <sogener@yandex.ru>
2022-06-09 22:13:02 +0500

    Range

--- main.go
+++ main.go
@@ -2,23 +2,10 @@ package main
 
 import "fmt"
 
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
 func main() {
-	var s []int
-	printSlice(s)
-
-	// append works on nil slices.
-	s = append(s, 0)
-	printSlice(s)
-
-	// The slice grows as needed.
-	s = append(s, 1)
-	printSlice(s)
-
-	// We can add more than one element at a time.
-	s = append(s, 2, 3, 4)
-	printSlice(s)
-}
-
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
