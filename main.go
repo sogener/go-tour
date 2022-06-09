bfe4cd466726235aa78ab35c5466f2248965459a
sogener <sogener@yandex.ru>
2022-06-09 22:03:37 +0500

    Slice length and capacity

--- main.go
+++ main.go
@@ -4,20 +4,21 @@ import "fmt"
 
 func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
 
-	s = s[1:4]
-	fmt.Println(s)
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
 
-	s = s[:2]
-	fmt.Println(s)
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
 
-	s = s[1:]
-	fmt.Println(s)
+	// Drop its first two values.
+	s = s[2:]
+	printSlice(s)
+}
 
-	/**
-	OUTPUT:
-	[3 5 7]
-	[3 5]
-	[5]
-	*/
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
