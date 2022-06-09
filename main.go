6786610a32c05bb236f2d16450dd31bf185b2b3b
sogener <sogener@yandex.ru>
2022-06-09 21:44:03 +0500

    If short version

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
 
 func main() {
-	value := 1
-
-	for value < 10 {
-		value += value
-	}
-
-	fmt.Println(value)
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 }
