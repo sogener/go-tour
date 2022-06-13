9fb0fac2f5bc29cfcade9a5518480854621aab0e
sogener <sogener@yandex.ru>
2022-06-13 17:39:43 +0500

    Pointers and functions

--- main.go
+++ main.go
@@ -10,8 +10,15 @@ func (s *Sentence) appendWords() string {
 	return s.firstWord + " " + s.secondWord
 }
 
+func changeWordsInSentence(s *Sentence, firstWord, secondWord string) {
+	s.firstWord = firstWord
+	s.secondWord = secondWord
+}
+
 func main() {
 	sentence := Sentence{firstWord: "hello", secondWord: "yopta"}
+	fmt.Println(sentence.appendWords())
 
+	changeWordsInSentence(&sentence, "poka", "cat")
 	fmt.Println(sentence.appendWords())
 }
