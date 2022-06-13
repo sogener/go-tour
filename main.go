25794b78d0e3ba6b02a6800f0128107ea6448aaa
sogener <sogener@yandex.ru>
2022-06-13 17:23:09 +0500

    Methods are functions Remember: a method is just a function with a receiver argument.
    Here's Abs written as a regular function with no change in functionality.

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ type Numbers struct {
 	numberOne, numberTwo int
 }
 
-func (n Numbers) sumNumbers() int {
+func sumNumbers(n Numbers) int {
 	return n.numberOne + n.numberTwo
 }
 
 func main() {
 	n := Numbers{numberTwo: 2, numberOne: 1}
 
-	fmt.Println(n.sumNumbers())
+	fmt.Println(sumNumbers(n))
 }
